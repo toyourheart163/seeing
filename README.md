526d5950cf9cf2af5383c29a6b4e0da4f85b12b1
toyourheart163 <blive200@gmail.com>
2020-03-18 19:35:12 +0800

    [A]extend cmd, node, sh...

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 support go, python, c++, c.
 filename endswith .py .c .cpp .go
+extend other.
 
 [zh_cn] 自动执行修改后的脚本
 
@@ -20,3 +21,11 @@ seeing hello.cpp      # it will run g++ hello.cpp && ./a.out
 seeing hello.c        # same as cpp
 ```
 
+## extend other
+
+```bash
+seeing -c sh hello.sh
+seeing -c open hello.html
+seeing -c node hello.js
+seeing -c scrapy spider.py
+```
--- seeing/__init__.py
+++ seeing/__init__.py
@@ -6,10 +6,12 @@ filename endswith .py .c .cpp .go
 """
 
 __author__ = 'Mikele'
-__version__ = '0.1.3'
+__version__ = '0.1.5'
 
-import os, time
+import os, time, logging
 import argparse
+
+logging.basicConfig(level=logging.INFO)
 
 help_text = 'monitor execute file every seconds.'
 parser = argparse.ArgumentParser(description=help_text + " support go cpp c py")
@@ -17,12 +19,17 @@ parser.add_argument(dest='filename', metavar='filename')
 parser.add_argument(
     '-s', '--seconds', metavar='seconds', action='store',
     default=1.0, help=help_text)
+parser.add_argument(
+    '-c', '--cmd', metavar='cmd', action='store', default='',
+    help="command to execute script, eg: seeing -c bash hello.sh")
 
-def monitor_file_modify_every(seconds, filename):
+commands = ['go', 'python', 'py', 'python3', 'g++', 'gcc']
+
+def monitor_file_modify_every(seconds, filename, command):
     st = os.stat(filename).st_mtime
     while True:
         f = os.stat(filename)
-        if st != f.st_mtime:
+        if st != f.st_mtime and command not in commands:
             st = f.st_mtime
             if filename.endswith('.cpp'):
                 cmd = "g++ {} && ./a.out"
@@ -32,7 +39,10 @@ def monitor_file_modify_every(seconds, filename):
                 cmd = "python {}"
             elif filename.endswith('.go'):
                 cmd = "go run {}"
+            else:
+                cmd = command + " {}"
             os.system(cmd.format(filename))
+            logging.info(cmd.format(filename))
         try:
             time.sleep(seconds)
         except KeyboardInterrupt:
